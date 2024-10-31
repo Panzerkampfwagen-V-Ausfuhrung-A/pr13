f3df644b59b8dae3650306aefebc862560bbe6be
Panzerkampfwagen-V-Ausfuhrung-A <158435288+Panzerkampfwagen-V-Ausfuhrung-A@users.noreply.github.com>
2024-10-31 15:16:50 +0300

    Отчёт и ПР

---
+++ чекушка/чекушка/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace чекушка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "пР_13_BD_Tur_FirmDataSet.Информация_о_туристах". При необходимости она может быть перемещена или удалена.
+            this.информация_о_туристахTableAdapter.Fill(this.пР_13_BD_Tur_FirmDataSet.Информация_о_туристах);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "пР_13_BD_Tur_FirmDataSet.Туристы". При необходимости она может быть перемещена или удалена.
+            this.туристыTableAdapter.Fill(this.пР_13_BD_Tur_FirmDataSet.Туристы);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            туристыTableAdapter.Update(пР_13_BD_Tur_FirmDataSet);
+            информация_о_туристахTableAdapter.Update(пР_13_BD_Tur_FirmDataSet);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
